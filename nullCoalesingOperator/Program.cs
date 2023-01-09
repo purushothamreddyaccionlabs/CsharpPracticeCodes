47ee3709c8ec7e4f4892682bfb320331eed0f580
purushothamreddyaccionlabs <121781414+purushothamreddyaccionlabs@users.noreply.github.com>
2023-01-09 10:05:01 +0530

    Add files via upload

---
+++ nullCoalesingOperator/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+class program
+{
+    static void Main()
+    {
+        int? TicketsOnSale = null;
+        int AvailableTickets = TicketsOnSale ?? 0;
+
+        Console.WriteLine("Available tickets is {0}", AvailableTickets);
+    }
+}
