415c3310003004eb875889b107092b08955e485d
purushothamreddyaccionlabs <121781414+purushothamreddyaccionlabs@users.noreply.github.com>
2023-01-09 10:08:28 +0530

    Add files via upload

---
+++ OutParameter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Xml.Schema;
+
+class program
+{
+    public static void Main()
+    {
+        calculate(20, 20, out int total, out int product);
+        Console.WriteLine(total);
+        Console.WriteLine(product);
+    }
+    public static void calculate(int FN,int SN, out int SUM,out int product)
+    {
+        SUM = FN+SN;
+        product = SN * FN;
+    }
+}
