11c64e0d960687bc3755214bf28ebeaa61978d6a
purushothamreddyaccionlabs <121781414+purushothamreddyaccionlabs@users.noreply.github.com>
2023-01-09 10:02:21 +0530

    Add files via upload

---
+++ Test9CsharpProject/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace csharpProject
+{
+    class program
+    {
+  
+        static void Main()
+        {
+            bool? areYouaMajor = null;  
+            if(areYouaMajor== true)
+            {
+                Console.WriteLine("You are major");
+            }else if(areYouaMajor== false) {
+                Console.WriteLine("You are minor");
+            }else
+            {
+                Console.WriteLine("You did not answer the question");
+            }
+        }
+    }
+}
