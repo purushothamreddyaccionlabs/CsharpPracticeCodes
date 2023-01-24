c1bfecb896a7169e0ea9b61a5f1cb77829f1b07a
purushothamreddyaccionlabs <purushotham.reddy@accionlabs.com>
2023-01-24 21:33:35 +0530

    24/01/2023

---
+++ MVCWebApplication1/MVCWebApplication1/Controllers/CategoryController1.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace MVCWebApplication1.Controllers
+{
+    public class CategoryController1 : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ MVCWebApplication1/MVCWebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MVCWebApplication1.Models;
+
+namespace MVCWebApplication1.Data
+{
+    public class ApplicationDbContext:DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options) 
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+    }
+}
