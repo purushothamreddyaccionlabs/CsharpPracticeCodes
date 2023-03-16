23cef7c91d7ed0edc56dd19c283db7f67f9bb1fb
purushothamreddyaccionlabs <purushotham.reddy@accionlabs.com>
2023-03-16 22:16:44 +0530

    16/03/2023

---
+++ DictioneryVsList E81/DictioneryVsList E81/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+class Program {
+
+    static void Main() 
+    {
+        Country country1 = new Country() { Code = "IND", Name = "INDIA",Capital = "New Delhi"};
+        Country country2 = new Country() { Code = "USA", Name = "UNITED STATES OF AMERICA", Capital = "Washington D.C." };
+        Country country3 = new Country() { Code = "CAN", Name = "CANADA", Capital = "Ottawa" };
+        Country country4 = new Country() { Code = "GBR", Name = "UNITED KINGDOM", Capital = "London" };
+        Country country5 = new Country() { Code = "UAE", Name = "UNITED ARAB EMIRATES", Capital = "Dubai" };
+
+
+
+        /* List<Country> list = new List<Country>();
+         list.Add(country1);
+         list.Add(country2);
+         list.Add(country3);
+         list.Add(country4);
+         list.Add(country5);*/
+
+        Dictionary<string, Country> dictionaryCountries = new Dictionary<string, Country>();
+        dictionaryCountries.Add(country1.Code, country1);
+        dictionaryCountries.Add(country2.Code, country2);
+        dictionaryCountries.Add(country3.Code, country3);
+        dictionaryCountries.Add(country4.Code, country4);
+        dictionaryCountries.Add(country5.Code, country5);
+
+        string userChoice = string.Empty;
+        do
+        {
+            Console.WriteLine("Please enter country code");
+            string userInput = Console.ReadLine().ToUpper();
+            //Country resultCountry = list.Find(country => country.Code == userInput);
+
+            Country resultCountry = dictionaryCountries.ContainsKey(userInput) ? dictionaryCountries[userInput] : null;
+            if (resultCountry == null)
+            {
+                Console.WriteLine("Invalid country code");
+            }
+            else
+            {
+                Console.WriteLine("Name = {0} Capital = {1}", resultCountry.Name, resultCountry.Capital);
+            }
+            do
+            {
+                Console.WriteLine("Do you want to continue-YES or NO ?");
+                userChoice = Console.ReadLine().ToUpper();
+            } while (userChoice != "NO" && userChoice != "YES");
+
+        } while (userChoice == "YES");
+    }
+}
+
+public class Country
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
+    public string Capital { get; set; }
+
+}
---
+++ GenericList Class and Range E76/GenericList Class and Range E76/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class Program
+{
+    public static void Main()
+    {
+
+
+        List<int> numbers = new List<int>() { 1, 5, 73, 83, 8, 2, 9, 10 };
+        numbers.Sort();
+       foreach(int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+        numbers.Reverse();
+        Console.WriteLine("numbers in descending order");
+        foreach(int num in numbers)
+        {
+            Console.WriteLine(num);
+        }
+
+
+
+        List<string> alphabets = new List<string>() { "A", "Z", "F", "J","Y","H" };
+        alphabets.Sort();
+        foreach(string alph in alphabets)
+        {
+            Console.WriteLine(alph);
+        }
+
+        alphabets.Reverse();
+        foreach (string alphs in alphabets)
+        {
+            Console.WriteLine(alphs);
+        }
+
+        Customer customer1 = new Customer()
+        {
+            Id = 101,
+            Name = "Rajesh",
+            Salary = 25000,
+            Type = "Retail Customer"
+        };
+        Customer customer2 = new Customer()
+        {
+            Id = 102,
+            Name = "Vinod",
+            Salary = 7690,
+            Type = "Retail Customer"
+        };
+        Customer customer3 = new Customer()
+        {
+            Id = 156,
+            Name = "Mahesh",
+            Salary = 5000,
+            Type = "Corporate Customer"
+        };
+        Customer customer4 = new Customer()
+        {
+            Id = 145,
+            Name = "Prasad",
+            Salary = 7910,
+            Type = "Corporate Customer"
+        };
+        Customer customer5 = new Customer()
+        {
+            Id = 196,
+            Name = "Kishore",
+            Salary = 9000,
+            Type = "Corporate Customer"
+        };
+
+        List<Customer> listcustomer = new List<Customer>();
+        listcustomer.Add(customer1);
+        listcustomer.Add(customer2);
+        listcustomer.Add(customer3);
+
+
+        /*        List<Customer> listcorporate = new List<Customer>();
+                listcorporate.Add(customer3);
+                listcorporate.Add(customer4);
+                listcorporate.Add(customer5);
+
+                listcustomer.AddRange(listcorporate);*/
+        /*
+                foreach(Customer c in listcustomer)
+                {
+                    Console.WriteLine("Id = {0}, Name = {1}, Salary = {2}, Type = {3}",c.Id,c.Name,c.Salary,c.Type);
+                }*/
+
+        /*List<Customer> customers = listcustomer.GetRange(0, 3);
+        foreach (Customer c in customers)
+        {
+            Console.WriteLine("Id = {0}, Name = {1}, Salary = {2}, Type = {3}", c.Id, c.Name, c.Salary, c.Type);
+        }*/
+
+        
+
+
+
+        Console.WriteLine("Before sorting");
+        foreach(Customer c in listcustomer)
+        {
+            Console.WriteLine(c.Name);
+        }
+        SortbyName sortbyname = new SortbyName();
+        listcustomer.Sort(sortbyname);
+
+        Console.WriteLine("After sorting");
+        foreach(Customer c in listcustomer)
+        {
+            Console.WriteLine(c.Name); 
+        }
+
+
+    }
+    public class SortbyName : IComparer<Customer>
+    {
+       public int Compare(Customer x, Customer y)
+        {
+            return x.Name.CompareTo(y.Name);
+        }
+    }
+}
+public class Customer/*:IComparable<Customer>*/
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public string Type { get; set; }
+
+ /*  public  int CompareTo(Customer other)
+    {
+        *//* if (this.Salary > other.Salary)
+             return 1;
+         else if (this.Salary < other.Salary)
+             return -1;
+         else
+             return 0;*/
+
+        /*        return this.Salary.CompareTo(other.Salary);*//*
+        return this.Name.CompareTo(other.Name);
+    }*/
+}
