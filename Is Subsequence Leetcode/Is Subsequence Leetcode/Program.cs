661d8588cd1958e5cac3ac1460df982d829629f0
purushothamreddyaccionlabs <purushotham.reddy@accionlabs.com>
2023-03-21 15:05:10 +0530

    Leetcode 21/03/23

---
+++ Is Subsequence Leetcode/Is Subsequence Leetcode/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    public static bool IsSubsequence(string s, string t)
+    {
+        int sIndex = 0;
+        int tIndex = 0;
+        while (sIndex < s.Length && tIndex < t.Length)
+        {
+            if (s[sIndex] == t[tIndex])
+            {
+                sIndex++;
+            }
+            tIndex++;
+        }
+        return sIndex == s.Length;
+    }
+    static void Main()
+    {
+        Console.WriteLine(IsSubsequence("abc","ahbgdc"));
+    }
+}
---
+++ Isomorphic Strings/Isomorphic Strings/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        string s = "egg";
+        string t = "add";
+        char[] str1 = s.ToCharArray();
+        char[] str2 = t.ToCharArray();
+        Dictionary<char, char> charCount = new Dictionary<char, char>();
+        char c = 'a';
+        // if(str1.Length != str2.Length) return false;
+        for (int i = 0; i < str1.Length; i++)
+        {
+            if (charCount.ContainsKey(str1[i]))
+            {
+                charCount.TryGetValue(str1[i], out c);
+                if (c != str2[i])
+                    Console.WriteLine(false); 
+            }
+            else if (!charCount.ContainsValue(str2[i]))
+            {
+                charCount.Add(str1[i], str2[i]);
+            }
+            else
+            {
+                Console.WriteLine(false);
+            }
+        }
+        Console.WriteLine(true);
+
+    }
+}
